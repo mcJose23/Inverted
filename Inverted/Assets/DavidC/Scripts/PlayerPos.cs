126e4765fc3f81c7fdbda8c075b279638aab48e7
D4V1D2306 <129797972+D4V1D2306@users.noreply.github.com>
2023-05-02 14:01:39 -0700

    new stufff

--- Inverted/Assets/DavidC/Scripts/PlayerPos.cs
+++ Inverted/Assets/DavidC/Scripts/PlayerPos.cs
@@ -17,7 +17,7 @@ public class PlayerPos : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.R))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
