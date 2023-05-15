dc853983e0b1b9b905f2fdcb1d8f39fa7d76aead
D4V1D2306 <129797972+D4V1D2306@users.noreply.github.com>
2023-05-15 13:53:45 -0700

    ahjadkfd

--- Inverted/Assets/DavidC/Scripts/Scorehandler.cs
+++ Inverted/Assets/DavidC/Scripts/Scorehandler.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Scorehandler : MonoBehaviour
 {
     public int CoinScore = 0;
-   
+    public TMP_Text ScoreText;
+    
+    private void Update()
+    {
+        ScoreText.text = "" + CoinScore;
+    }
     // Update is called once per frame
     public void FoundCoin()
     {
