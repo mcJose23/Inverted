e28edd59eb733c63012971ccf2c30b50ed622cd9
mcJose23 <mcastillo4002@gmail.com>
2023-05-17 14:58:40 -0700

    sbfeikuwswgbef

--- Inverted/Assets/DavidC/Scripts/FollowPlayer.cs
+++ Inverted/Assets/DavidC/Scripts/FollowPlayer.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Coin : MonoBehaviour
+public class FollowPlayer : MonoBehaviour
 {
+    public Transform player;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class Coin : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        gameObject.transform.position = new Vector3(player.position.x, player.position.y, gameObject.transform.position.z);
     }
 }
