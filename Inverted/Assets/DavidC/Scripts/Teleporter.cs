64661c24364f845f53a4716a9d8553376a3eb086
D4V1D2306 <129797972+D4V1D2306@users.noreply.github.com>
2023-05-25 13:13:46 -0700

    Finalest

--- Inverted/Assets/DavidC/Scripts/Teleporter.cs
+++ Inverted/Assets/DavidC/Scripts/Teleporter.cs
@@ -5,14 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class Teleporter : MonoBehaviour
 {
-    public float x;
-    public float y;
+   
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player")) 
         {
-            other.transform.position = new Vector2(x, y);
+            PlayGame();
         }
     }
     public void PlayGame()
